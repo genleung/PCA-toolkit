51ac7366c6d64a5b0a9d02890158a41f00fe5650
genleung <genleung@hotmail.com>
2017-03-17 09:10:44 +0800

    Add searchFiles() function and remove redundant includes (utils.h)

--- pca.cpp
+++ pca.cpp
@@ -19,16 +19,16 @@ bool parseCommandLine(int argc, char* argv[]){
     std::shared_ptr<TCLAP::ValueArg<int>> classFrom, classTo; // 类label区间表达：[classFrom, classTo] (闭区间)
     std::shared_ptr<TCLAP::ValueArg<int>> width, height; // 对样本进行缩放，此处指定缩放后的尺寸；若为非正值，则不缩放
     std::shared_ptr<TCLAP::ValueArg<int>> grayscaled; // 灰度化
-    std::shared_ptr<TCLAP::ValueArg<int>> contrastStretched; // 对比度拉伸    
-    std::shared_ptr<TCLAP::ValueArg<int>> binarizedType; // 对样本进行二值化：0不二值化; 1 otsu二值化; 2 adaptive二值化  
+    std::shared_ptr<TCLAP::ValueArg<int>> contrastStretched; // 对比度拉伸
+    std::shared_ptr<TCLAP::ValueArg<int>> binarizedType; // 对样本进行二值化：0不二值化; 1 otsu二值化; 2 adaptive二值化
     std::shared_ptr<TCLAP::ValueArg<float>> reservedVariance;
     std::shared_ptr<TCLAP::ValueArg<std::string>> trainSamplesPath;
     std::shared_ptr<TCLAP::ValueArg<std::string>> testSamplesPath;
     std::shared_ptr<TCLAP::ValueArg<std::string>> pcaPath;
     std::shared_ptr<TCLAP::ValueArg<int>> edgeWidth;
     std::shared_ptr<TCLAP::SwitchArg> preview;
-    std::shared_ptr<TCLAP::SwitchArg> verbose;    
-    
+    std::shared_ptr<TCLAP::SwitchArg> verbose;
+
     if(param==nullptr) return false;
 
     try{
@@ -39,15 +39,15 @@ bool parseCommandLine(int argc, char* argv[]){
         pcaPath=std::make_shared<TCLAP::ValueArg<std::string>>("P", "pca", "Path of PCA model", false, DEFAULT_PCA_PATH, "PCA_file_path", cmd);
 
         preview=std::make_shared<TCLAP::SwitchArg>("p", "preview", "Preview the sample for test", cmd, false);
-        verbose=std::make_shared<TCLAP::SwitchArg>("v", "verbose", "Display the verbose log messages", cmd, false);        
-        height=std::make_shared<TCLAP::ValueArg<int>>("H", "height", "Scaled sample height", false, 0, "scaled_sample_height", cmd);        
+        verbose=std::make_shared<TCLAP::SwitchArg>("v", "verbose", "Display the verbose log messages", cmd, false);
+        height=std::make_shared<TCLAP::ValueArg<int>>("H", "height", "Scaled sample height", false, 0, "scaled_sample_height", cmd);
         width=std::make_shared<TCLAP::ValueArg<int>>("W", "width", "Scaled sample width", false, 0, "scaled_sample_width", cmd);
         binarizedType=std::make_shared<TCLAP::ValueArg<int>>("b", "binarize", "Binarise the sample image", false, 0, "binarise_image", cmd);
         edgeWidth=std::make_shared<TCLAP::ValueArg<int>>("e", "edge", "Remove edge before applying PCA", false, 0, "edge_width_to_remove", cmd);
         grayscaled=std::make_shared<TCLAP::ValueArg<int>>("g", "grayscale", "Grayscale image before applying PCA", false, 1, "load_images_in_grayscale", cmd);
-        contrastStretched=std::make_shared<TCLAP::ValueArg<int>>("c", "contrast", "Stretching contrast before applying grayscaling & PCA", false, 0, "contrast_stretch", cmd);        
-        reservedVariance=std::make_shared<TCLAP::ValueArg<float>>("V", "variance", "Reserved variance, ranging in (0,1.0], but 0.95~0.99 is normallly a good option", false, DEFAULT_RESERVED_VARIANCE, "reserved_variance", cmd);        
-        classTo=std::make_shared<TCLAP::ValueArg<int>>("t", "to_class_label", "Classes label id to which to train", true, 0, "class_to", cmd);        
+        contrastStretched=std::make_shared<TCLAP::ValueArg<int>>("c", "contrast", "Stretching contrast before applying grayscaling & PCA", false, 0, "contrast_stretch", cmd);
+        reservedVariance=std::make_shared<TCLAP::ValueArg<float>>("V", "variance", "Reserved variance, ranging in (0,1.0], but 0.95~0.99 is normallly a good option", false, DEFAULT_RESERVED_VARIANCE, "reserved_variance", cmd);
+        classTo=std::make_shared<TCLAP::ValueArg<int>>("t", "to_class_label", "Classes label id to which to train", true, 0, "class_to", cmd);
         classFrom=std::make_shared<TCLAP::ValueArg<int>>("f", "from_class_label", "Classes label id from which to train", true, 0, "class_from", cmd);
 
         cmd.parse(argc, argv);
@@ -55,7 +55,7 @@ bool parseCommandLine(int argc, char* argv[]){
         LOGL(ERROR)<<e.error().c_str()<<" for arg " << e.argId();
         return false;
     }
-    
+
 
     param->classFrom=classFrom->getValue();
     param->classTo=classTo->getValue();
@@ -71,7 +71,7 @@ bool parseCommandLine(int argc, char* argv[]){
     param->edgeWidth=edgeWidth->getValue();
     param->preview=preview->getValue();
     param->verbose=verbose->getValue();
-    
+
     // 设置Log级别
     if(param->verbose){
         art::setLogLevel(art::LogLevel::INFO);
@@ -84,7 +84,7 @@ bool parseCommandLine(int argc, char* argv[]){
         LOGL(ERROR)<<"Classes' count should be larger than 2 !";
        return false;
     }
-    
+
     if(param->reservedVariance<=0.0 || param->reservedVariance>1.0 ){
         LOGL(ERROR)<<"Reserved variance value should be ranged in ( 0, 1.0 ]";
         return false;
@@ -95,14 +95,50 @@ bool parseCommandLine(int argc, char* argv[]){
     LOG()<<"Train-samples path: "<< param->trainSamplesPath;
     LOG()<<"Test-samples path: "<< param->testSamplesPath;
     LOG()<<"PCA model path: "<< param->pcaPath;
-    
+
     return true;
 }
 
+/** 获取指定目录下的文件列表。需要libboost支持。
+ * @param dir 指定目录
+ * @param recursive 是否递归寻找
+ * @param needSort 对列表是否进行排序
+ * @filenames 保存的文件列表结果
+ */
+void searchFiles(const std::string dir, std::vector<std::string>& filenames, bool recursive=false, bool needSort=false){
+    auto sortByName = [](std::string name1, std::string name2){
+        return name1 < name2;
+    };
+
+    fs::path path(dir);
+
+    if (!fs::exists(path)){
+        DLOGL(ERROR, "Invalid search path!");
+        return;
+    }
+
+    // Converts path, which must exist, to an absolute path that has no symbolic link, dot, or dot-dot elements.
+    fs::path canonicaled_path=fs::canonical(path);
+    fs::directory_iterator end_iter;
+    for (fs::directory_iterator iter(canonicaled_path); iter!=end_iter; ++iter){
+        if (fs::is_regular_file(iter->status())){
+            filenames.push_back(iter->path().string());
+        }
+
+        if (recursive && fs::is_directory(iter->status())){
+            searchFiles(iter->path().string(), filenames, needSort, recursive);
+        }
+    }
+
+    if(needSort){
+        std::sort(filenames.begin(), filenames.end(), sortByName);
+    }
+
+}
 
 /** 对比度拉伸
- *  
- * 
+ *
+ *
  */
 cv::Mat contrastStretching(const cv::Mat& inputImage, double minVal, double maxVal)
 {
@@ -116,7 +152,7 @@ cv::Mat contrastStretching(const cv::Mat& inputImage, double minVal, double maxV
         if(temp<0) temp=0;
         lut.at<uchar>(i)=temp;
 	}
-    
+
     if(inputImage.channels()>=3){
         cv::Mat gray;
         cv::cvtColor(inputImage, gray, CV_BGR2GRAY);
@@ -136,13 +172,13 @@ int processSamples(std::string path, std::vector<cv::Mat>& processedSamples){
     cv::Mat img;
     cv::Mat preview;
     int previewCount=0;
-    
-    if (!fs::exists(fs::path(path))){  
-        LOGL(ERROR)<<"Sample path ("<<path<<") not exist!";
-        return 0; 
-    }  
 
-    art::searchFiles(path, sampleFiles);
+    if (!fs::exists(fs::path(path))){
+        LOGL(ERROR)<<"Sample path ("<<path<<") not exist!";
+        return 0;
+    }
+
+    searchFiles(path, sampleFiles);
 
 
     for(auto& f : sampleFiles){
@@ -157,46 +193,46 @@ int processSamples(std::string path, std::vector<cv::Mat>& processedSamples){
             LOGL(ERROR)<<"Failed to read image.";
             continue;
         }
-        
+
         //// 检查是否缩放
         if(g_param.scaledHeight>0 && g_param.scaledWidth>0){
             cv::Mat temp(cv::Size(g_param.scaledWidth, g_param.scaledHeight), img.type());
-            cv::resize(img, temp, temp.size(), cv::INTER_AREA); // generally, it's a shrink op 
+            cv::resize(img, temp, temp.size(), cv::INTER_AREA); // generally, it's a shrink op
             img=temp;
         }
-                
+
         //// 检查是否要移除边缘
         if(g_param.edgeWidth>0){
-            cv::Mat temp=img(cv::Rect(g_param.edgeWidth,g_param.edgeWidth, img.cols-2*g_param.edgeWidth, img.rows-2*g_param.edgeWidth));      
-            img=temp;      
+            cv::Mat temp=img(cv::Rect(g_param.edgeWidth,g_param.edgeWidth, img.cols-2*g_param.edgeWidth, img.rows-2*g_param.edgeWidth));
+            img=temp;
         }
-              
+
         //// 注意，进行简单的预处理未必能够提升PCA的特征抽取能力；相反，可能降低！
         //// 因为预处理可能把具有区分度的特征抛弃掉！
-        
+
         //// 下面是对样本图像进行对比度拉伸。
         if(g_param.contrastStretched){
-            double minVal, maxVal; 
+            double minVal, maxVal;
             cv::minMaxLoc(img, &minVal, &maxVal);
             cv::Mat temp=contrastStretching(img, minVal, maxVal);
             img=temp;
         }
-        
+
         //// 下面是另一个简单的预处理：
-        //// 把灰度图阈值化转为黑白两色图像。实验证明，阈值化后的图像PCA降维 
-        if(img.type()==CV_8UC1){   
+        //// 把灰度图阈值化转为黑白两色图像。实验证明，阈值化后的图像PCA降维
+        if(img.type()==CV_8UC1){
             if(g_param.binarizedType==1){
                 cv::threshold(img, img, 0, 255, cv::THRESH_BINARY|cv::THRESH_OTSU);
             }else if(g_param.binarizedType==2){
                 cv::adaptiveThreshold(img, img, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 5, 9);
             }
         }
-        
+
         if(g_param.preview){
             if(preview.data==nullptr){
                 preview=cv::Mat(cv::Size(img.cols*g_param.previewWidth, img.rows*g_param.previewHeight), img.type(), cv::Scalar(0));
             }
-            
+
             cv::Mat roi=preview(cv::Rect(img.cols*(previewCount%g_param.previewWidth), img.rows*(previewCount/g_param.previewWidth), img.cols, img.rows));
             img.copyTo(roi);
             previewCount++;
@@ -208,7 +244,7 @@ int processSamples(std::string path, std::vector<cv::Mat>& processedSamples){
                 previewCount=0;
                 preview.release();
             }
-            
+
         }
 
         // 保存经过预处理的样本
@@ -257,19 +293,19 @@ bool loadSamples(std::vector<cv::Mat>& trainSamples, std::vector<cv::Mat>& testS
         }else{
             LOGL(INFO)<<"Class-"<<i<<" "<<n<<" testing-samples loaded.";
         }
-        param->testSamplesCountInClasses.push_back(n);        
+        param->testSamplesCountInClasses.push_back(n);
     }
 
     // 把训练集和测试集汇总到一起
     for(auto & e : trainSamples){
         allSamples.push_back(e);
-    }    
+    }
     for(auto& e: testSamples){
         allSamples.push_back(e);
     }
 
     return true;
-   
+
 }
 
 
@@ -309,7 +345,7 @@ bool generateLIBSVMData(std::string filename,  std::vector<cv::Mat>& samples, st
     std::fstream file;
     std::stringstream ss;
 
-    // 打开文件准备存储        
+    // 打开文件准备存储
     file.open(filename, std::ios::out);
     if(!file.is_open()){
         LOGL(ERROR)<<"Failed to write data file!";
@@ -326,7 +362,7 @@ bool generateLIBSVMData(std::string filename,  std::vector<cv::Mat>& samples, st
         return false;
     }
 
-    // 保存样本到数据文件    
+    // 保存样本到数据文件
     for(int r=0; r<compressed.rows; r++){
         ss.clear();
         ss.str("");
@@ -358,7 +394,7 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    // 载入样本, 每个样本都是一个图像cv::Mat    
+    // 载入样本, 每个样本都是一个图像cv::Mat
     if(!loadSamples(trainSamples, testSamples, allSamples)){
         LOGL(ERROR)<<"Failed to load samples!";
         return -1;
@@ -377,7 +413,7 @@ int main(int argc, char** argv){
     }else{
         LOG()<<"Test-samples count: "<<testSamples.size();
     }
-        
+
     // 从总样本集计算PCA模型
     if(!generatePCAModel(allSamples, pca)){
         LOGL(ERROR)<<"Failed to generate PCA model!";
@@ -395,21 +431,21 @@ int main(int argc, char** argv){
     cv::FileStorage storage(pcaFile, cv::FileStorage::WRITE);
     pca.write(storage);
     storage.release();
-    LOGL(NOTICE)<<"PCA model saved to "<<pcaFile;   
+    LOGL(NOTICE)<<"PCA model saved to "<<pcaFile;
     LOGL(NOTICE)<<"Samples' dimension could be reduced to " << pca.eigenvalues.rows <<" when reserving "<<g_param.reservedVariance*100<<"% variance.";
 
     // 为训练集和测试集生成label
     std::vector<int> trainLabels, testLabels;
     createLabels(g_param.trainSamplesCountInClasses, trainLabels);
     createLabels(g_param.testSamplesCountInClasses, testLabels);
-       
+
     // 生成训练集和测试集文件（LIBSVM使用的数据格式）
     LOG()<<"Saving train & test samples into LIBSVM data format...";
     bool ret;
     std::string trainSamplesFile=g_param.trainSamplesPath+"/train_c"+std::to_string(g_param.classFrom)+"-"+std::to_string(g_param.classTo)+"v"+std::to_string((int)(g_param.reservedVariance*100));
     if(g_param.edgeWidth>0){
         trainSamplesFile+="_noEdge";
-    }    
+    }
     trainSamplesFile+=".dat";
     ret=generateLIBSVMData(trainSamplesFile, trainSamples, trainLabels, pca);
     if(ret){
@@ -418,17 +454,17 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    std::string testSamplesFile=g_param.testSamplesPath+"/test_c"+std::to_string(g_param.classFrom)+"-"+std::to_string(g_param.classTo)+"v"+std::to_string((int)(g_param.reservedVariance*100)); 
+    std::string testSamplesFile=g_param.testSamplesPath+"/test_c"+std::to_string(g_param.classFrom)+"-"+std::to_string(g_param.classTo)+"v"+std::to_string((int)(g_param.reservedVariance*100));
     if(g_param.edgeWidth>0){
         testSamplesFile+="_noEdge";
-    }    
-    testSamplesFile+=".dat";   
+    }
+    testSamplesFile+=".dat";
     ret=generateLIBSVMData(testSamplesFile, testSamples, testLabels, pca);
     if(ret){
         LOGL(NOTICE)<<"Test-samples data file saved to "<<testSamplesFile;
     }else{
         return -1;
-    }    
+    }
 
     return 0;
 }
--- pca.h
+++ pca.h
@@ -1,15 +1,11 @@
 #include <string>
 #include <vector>
 #include <memory>
-#include <boost/filesystem.hpp>  
+#include <boost/filesystem.hpp>
 #include <opencv2/opencv.hpp>
 
 // libtclap 用来解析命令行参数
-#include "tclap/CmdLine.h" 
-
-// 需要的SDK头文件
-#include "../../Utils/utils.h"
-#include "../../ML/DataProcessing.h"
+#include "tclap/CmdLine.h"
 
 struct Parameters{
     int classFrom; ///< 类的开始id
@@ -19,7 +15,7 @@ struct Parameters{
     bool verbose;
     bool grayscaled; ///< 是否对样本图像进行灰度化
     bool contrastStretched; ///< 是否对样本进行对比度拉伸
-    int binarizedType; ///< 对样本图像进行二值化, 仅对灰度图像有效; 0不二值化; 1 otsu二值化; 2 adaptive二值化  
+    int binarizedType; ///< 对样本图像进行二值化, 仅对灰度图像有效; 0不二值化; 1 otsu二值化; 2 adaptive二值化
     float reservedVariance; ///< PCA保留的方差
     int edgeWidth; ///< 边缘是否移除（比如移除2个像素的周围一圈边缘）
     bool preview; ///< 是否预览样本(仅在测试时使用)
@@ -29,5 +25,5 @@ struct Parameters{
     std::string testSamplesPath; ///< 测试样本的路径
     std::string pcaPath; ///< PCA模型的保存路径
     std::vector<int> trainSamplesCountInClasses; ///< 训练集中各个类的样本数量
-    std::vector<int> testSamplesCountInClasses; ///< 测试集中各个类的样本数量  
+    std::vector<int> testSamplesCountInClasses; ///< 测试集中各个类的样本数量
 };
